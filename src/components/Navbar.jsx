d1df2e2b7889209b8f5073acd35ead7d92772101
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-02-16 14:17:31 +0700

    menambahkan popper ke dalam project,membuat navbar bagian kiri dan menambah logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>hello</div>;
+  return (
+    <section className="hero-section">
+      <div className="container">
+        <nav className="nav-custom">
+          <div className="nav-left">
+            <div className="nav-logo">
+              <img src="/assets/img/frog.svg" alt="" />
+            </div>
+            <div className="nav-links-custom">
+              <div className="nav-link-custom">
+                <a href="" className="link-secondary fw-bold">
+                  Beranda
+                </a>
+                <a href="" className="link-secondary fw-bold">
+                  Silabus
+                </a>
+                <a href="" className="link-secondary fw-bold">
+                  Testimonial
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </section>
+  );
 };
 
 export default Navbar;
