a4a6882a09fd71a28f1134f964a91f6589b1b09d
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-02-17 17:20:19 +0700

    penambahan

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,9 @@ const Hero = () => {
       {" "}
       <div className="row">
         <div className="col-lg-6">
-          <h1 className="h1-custom">Pelatihan Parenting Terbaik di Indonesia</h1>
+          <h1 className="h1-custom">
+            Pelatihan <i>Parenting</i> Terbaik di Indonesia
+          </h1>
           <p className="p-secondary">Mari kita sadar akan pentingnya pelatihan untuk menjadi orang tua agar menghasilkan karakter anak yang unggul.</p>
           <button className="btn-primary-custom">Daftar Sekarang</button>
         </div>
