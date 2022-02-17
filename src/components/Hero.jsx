a0b5d6ef1818f54b351fc36c013bbb36ffb6186f
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-02-17 17:08:52 +0700

    penambahan Button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,7 @@ const Hero = () => {
       <div className="col-lg-6">
         <h1 className="h1-custom">Lorem ipsum dolor sit amet.</h1>
         <p className="p-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nulla eligendi, nobis vero consectetur velit?</p>
+        <button className="btn-primary-custom">Daftar Sekarang</button>
       </div>
     </div>
   );
