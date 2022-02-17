825d92b7e87631cd21ffd8a466d45a3655027d86
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-02-17 17:07:41 +0700

    membuat sebuah row dgn colum 6 diukuran large,h1 dan p

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="row">
+      <div className="col-lg-6">
+        <h1 className="h1-custom">Lorem ipsum dolor sit amet.</h1>
+        <p className="p-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nulla eligendi, nobis vero consectetur velit?</p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
