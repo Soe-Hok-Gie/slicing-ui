5774b05f764b75b091a5746041c6e2860693dd31
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-02-17 17:26:39 +0700

    penambahan margin top pada button kedua

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             Pelatihan <i>Parenting</i> Terbaik di Indonesia
           </h1>
           <p className="p-secondary">Mari kita sadar akan pentingnya pelatihan untuk menjadi orang tua agar menghasilkan karakter anak yang unggul.</p>
-          <button className="btn-primary-custom">Daftar Sekarang</button>
+          <button className="btn-primary-custom mt-3">Daftar Sekarang</button>
         </div>
       </div>
     </div>
