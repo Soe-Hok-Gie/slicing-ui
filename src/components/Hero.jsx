248bead640536029c7516ca145cd6248c6f01799
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-02-17 17:15:29 +0700

    styling h1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import "./Style.css";
 
 const Hero = () => {
   return (
-    <div className="row">
-      <div className="col-lg-6">
-        <h1 className="h1-custom">Lorem ipsum dolor sit amet.</h1>
-        <p className="p-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nulla eligendi, nobis vero consectetur velit?</p>
-        <button className="btn-primary-custom">Daftar Sekarang</button>
+    <div className="container">
+      {" "}
+      <div className="row">
+        <div className="col-lg-6">
+          <h1 className="h1-custom">Lorem ipsum dolor sit amet.</h1>
+          <p className="p-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nulla eligendi, nobis vero consectetur velit?</p>
+          <button className="btn-primary-custom">Daftar Sekarang</button>
+        </div>
       </div>
     </div>
   );
